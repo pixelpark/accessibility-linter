0ab7f3d4aca8f9e99109b6f32d836fe7cc8fd641
GitHub Action <action@github.com>
2022-06-23 11:58:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bucherfa/accessibilitylinter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bucherfa.accessibilitylinter.services
+
+import com.github.bucherfa.accessibilitylinter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bucherfa/accessibilitylinter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bucherfa/accessibilitylinter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bucherfa.accessibilitylinter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bucherfa.accessibilitylinter.MyBundle
 
 class MyProjectService(project: Project) {
 
