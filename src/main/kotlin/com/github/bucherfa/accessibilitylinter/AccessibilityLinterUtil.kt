24318b13d78aa178e817fd10acbd522925a4ad11
Fabian Bucher <git@fabianbucher.de>
2022-06-30 07:37:50 +0200

    annotators: HTML annotator
    * executing external script

---
+++ src/main/kotlin/com/github/bucherfa/accessibilitylinter/AccessibilityLinterUtil.kt
@@ -0,0 +1,7 @@
+package com.github.bucherfa.accessibilitylinter
+
+class AccessibilityLinterUtil {
+    companion object {
+        val PACKAGE_NAME = "accessibility-linter"
+    }
+}
--- src/main/kotlin/com/github/bucherfa/accessibilitylinter/annotators/HtmlAnnotator.kt
+++ src/main/kotlin/com/github/bucherfa/accessibilitylinter/annotators/HtmlAnnotator.kt
@@ -1,36 +1,66 @@
 package com.github.bucherfa.accessibilitylinter.annotators
 
+import com.github.bucherfa.accessibilitylinter.AccessibilityLinterUtil
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.util.ExecUtil
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.ExternalAnnotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.application.PluginPathManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
+import java.io.File
+import java.nio.charset.Charset
 
+class CollectedInformation(val temporaryFilePath: String, val file: PsiFile)
 class CustomAnnotation(val range: TextRange, val message: String) {
 }
 
-class HtmlAnnotator : ExternalAnnotator<PsiFile, List<CustomAnnotation>>() {
+class HtmlAnnotator : ExternalAnnotator<CollectedInformation, List<CustomAnnotation>>() {
 
-    override fun collectInformation(file: PsiFile): PsiFile {
-        return file
+    override fun collectInformation(file: PsiFile, editor: Editor, hasErrors: Boolean): CollectedInformation? {
+        val content = file.text
+        val pluginPath = PluginPathManager.getPluginHomePath(AccessibilityLinterUtil.PACKAGE_NAME)
+        println("pluginPath ci: $pluginPath")
+        val pluginDirectory = File(pluginPath)
+        val temporaryFile = File.createTempFile("temp.", ".html")
+        val temporaryFilePath = temporaryFile.absolutePath
+        File(temporaryFilePath).appendText(content)
+        val command = listOf("node", "/home/pixel/.nvm/versions/node/v16.14.0/lib/node_modules/accessibility-checker/bin/achecker.js","--reportLevels","violation","--outputFormat","json","--policies","IBM_Accessibility,WCAG_2_1", "--outputFolder","/tmp", temporaryFilePath)
+        //val command = listOf("achecker", "--reportLevels violation --outputFormat json --policies IBM_Accessibility,WCAG_2_1,WCAG_2_0", temporaryFilePath)
+        val generalCommandLine = GeneralCommandLine(command)
+        generalCommandLine.charset = Charset.forName("UTF-8")
+        //generalCommandLine.workDirectory = pluginDirectory
+        val result = ExecUtil.execAndGetOutput(generalCommandLine)
+        print(result.stdout)
+        File(temporaryFilePath).delete()
+        return CollectedInformation(temporaryFilePath, file)
     }
 
-    override fun collectInformation(file: PsiFile, editor: Editor, hasErrors: Boolean): PsiFile {
-        return file
-    }
-
-    override fun doAnnotate(collectedInfo: PsiFile?): List<CustomAnnotation>? {
+    override fun doAnnotate(collectedInfo: CollectedInformation?): List<CustomAnnotation>? {
+//        val temporaryFilePath = collectedInfo?.temporaryFilePath
+//        if (temporaryFilePath != null) {
+//            val command = listOf("achecker --reportLevels violation --outputFormat json --policies IBM_Accessibility,WCAG_2_1,WCAG_2_0", temporaryFilePath)
+//            val pluginPath = PluginPathManager.getPluginHomePath(AccessibilityLinterUtil.PACKAGE_NAME)
+//            println("pluginPath da: $pluginPath")
+//            val pluginDirectory = File(pluginPath)
+//            val generalCommandLine = GeneralCommandLine(command)
+//            generalCommandLine.charset = Charset.forName("UTF-8")
+//            //generalCommandLine.workDirectory = pluginDirectory
+//            val result = ExecUtil.execAndGetOutput(generalCommandLine)
+//            print(result.stdout)
+//            File(temporaryFilePath).delete()
+//        }
         val annotations: MutableList<CustomAnnotation> = mutableListOf()
-        val content = collectedInfo!!.text
-        println(content)
-        val startIndex = content.indexOf("Hello World")
-        println(startIndex)
-        if (startIndex > 0) {
-            val newAnnotation = CustomAnnotation(TextRange(startIndex, startIndex + "Hello World".length), "Please include the whole universe! ;)")
-            annotations.add(newAnnotation)
-        }
-        println("Length: " + annotations.size)
+//        val startIndex = content.indexOf("Hello World")
+//        println(startIndex)
+//        if (startIndex > 0) {
+//            val newAnnotation = CustomAnnotation(TextRange(startIndex, startIndex + "Hello World".length), "Please include the whole universe! ;)")
+//            annotations.add(newAnnotation)
+//        }
+//        println("Length: " + annotations.size)
+//        println("result: " + result.stdout)
         return annotations
         //return super.doAnnotate(collectedInfo)
     }
